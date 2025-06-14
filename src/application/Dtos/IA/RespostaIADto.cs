dbf697d64cff1fb22ae067a93afd8f843869c278
Bruno <bbrunosiqueira14@gmail.com>
2025-06-14 03:20:46 -0300

    feat:ia

--- src/application/Dtos/IA/RespostaIADto.cs
+++ src/application/Dtos/IA/RespostaIADto.cs
@@ -2,7 +2,7 @@ namespace application.Dtos.IA;
 
 public class RespostaIADto
 {
-    public string Score { get; set; }
+    public int Score { get; set; }
     public string Descricao { get; set; }
     public string QuestionarioSlug { get; set; }
 }
